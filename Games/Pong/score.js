a10ac0f100ea0c270e1700ea6cc8e183b88158a5
Pyan <62762243+PY44N@users.noreply.github.com>
2020-09-14 12:43:40 -0400

    Added Comments

--- Games/Pong/score.js
+++ Games/Pong/score.js
@@ -1,24 +1,29 @@
 class Score {
     constructor() {
+        //Creates Variables
         this.score = 0
         this.x = windowWidth / 2
         this.y = windowHeight / 10
         this.size = 40
     }
 
+    //Write score to screen on Update
     update() {
         textSize(this.size)
         text(this.score.toString(), this.x, this.y)
     }
 
+    //Add Score
     add() {
         this.score++
     }
 
+    //Reset Score
     reset() {
         this.score = 0
     }
 
+    //Resize Element
     resize() {
         this.x = windowWidth / 2
         this.y = windowHeight / 10
