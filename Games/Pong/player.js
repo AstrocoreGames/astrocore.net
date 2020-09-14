73558112599c9f5d24a1713ca7cd436b68213355
Pyan <62762243+PY44N@users.noreply.github.com>
2020-09-14 12:46:19 -0400

    Added Comments

--- Games/Pong/player.js
+++ Games/Pong/player.js
@@ -1,5 +1,6 @@
 class Player {
     constructor() {
+        //Creates Variables
         this.x = windowWidth - (windowWidth / 40) - 10
         this.y = windowHeight / 2
         this.width = windowWidth / 40
@@ -8,25 +9,31 @@ class Player {
     }
 
     update() {
+        //If above screen
         if (this.y <= 0) {
             this.y = 0
         }
 
+        //If below screen
         if (this.y >= windowHeight - this.height) {
             this.y = windowHeight - this.height
         }
 
+        //Draws Player
         rect(this.x, this.y, this.width, this.height)
     }
 
+    //Moves Player Up
     up() {
         this.y -= this.speed
     }
 
+    //Moves Player Down
     down() {
         this.y += this.speed
     }
 
+    //Resizes Element
     resize() {
         this.x = windowWidth - (windowWidth / 40) - 10
         this.y = windowHeight / 2
