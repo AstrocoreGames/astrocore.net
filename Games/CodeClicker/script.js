1f685ff8e0080a72dc444cf753da24a9958a1a45
Pyan <Pyan4444@gmail.com>
2020-10-16 18:04:34 -0400

    Added data saving to code clicker

--- Games/CodeClicker/script.js
+++ Games/CodeClicker/script.js
@@ -1,106 +1,125 @@
-//Variables being declared
-var score = 0;
-var clickLevel = 1;
-var upgradeCost = 50;
-var coderCost = 1000;
-var superUpgradeCost = 750;
-var coders = 0;
-var coderLevel = 1;
-var coderUpgradeCost = 1250;
-var rebirths = 1;
-var rebirthCost = 1000000;
+let data
+
+if (getCookie("GameData") != "" && getCookie("GameData") != null) {
+  data = JSON.parse(atob(getCookie("GameData")))
+  refreshHTML()
+} else {
+  data = {}
+  data.score = 0;
+  data.clickLevel = 1;
+  data.upgradeCost = 50;
+  data.coderCost = 1000;
+  data.superUpgradeCost = 750;
+  data.coders = 0;
+  data.coderLevel = 1;
+  data.coderUpgradeCost = 1250;
+  data.rebirths = 1;
+  data.rebirthCost = 1000000;
+}
+
+//Totally not stolen code from the internet for cookies :)
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
 function addScore() {
-  score = score + clickLevel * rebirths;
+  data.score = data.score + data.clickLevel * data.rebirths;
   refreshHTML()
 }
 
 function upgradeClick() {
-  if (score >= upgradeCost) {
-    clickLevel++
-    score = score - upgradeCost
-    upgradeCost = upgradeCost +10;
+  if (data.score >= data.upgradeCost) {
+    data.clickLevel++
+    data.score = data.score - data.upgradeCost
+    data.upgradeCost = data.upgradeCost +10;
     refreshHTML()
   }
 }
 
 function superUpgrade() {
-  if (score >= superUpgradeCost) {
-    clickLevel = clickLevel +10;
-    score = score - superUpgradeCost
-    superUpgradeCost = superUpgradeCost +250;
+  if (data.score >= data.superUpgradeCost) {
+    data.clickLevel = data.clickLevel +10;
+    data.score = data.score - data.superUpgradeCost
+    data.superUpgradeCost = data.superUpgradeCost +250;
     refreshHTML()
   }
 }
 
 function buyClicker() {
-  if (score >= coderCost) {
-    coders++
-    score = score - coderCost
-    coderCost = coderCost +200;
+  if (data.score >= data.coderCost) {
+    data.coders++
+    data.score = data.score - data.coderCost
+    data.coderCost = data.coderCost +200;
     refreshHTML()
     gameLoop()
   }
 }
 
-/*function buyClickerAll() {
-  while (score >= coderCost) {
-    buyClicker()
-  }
-}*/
-
 function upgradeCoder() {
-  if (score >= coderUpgradeCost) {
-    coderLevel++
-    score = score - coderUpgradeCost
-    coderUpgradeCost = coderUpgradeCost +300;
+  if (data.score >= data.coderUpgradeCost) {
+    data.coderLevel++
+    data.score = data.score - data.coderUpgradeCost
+    data.coderUpgradeCost = data.coderUpgradeCost +300;
     refreshHTML()
   }
 }
 
-/*function upgradeCoderAll() {
-  while (score >= coderUpgradeCost) {
-    upgradeCoder()
-  }
-}*/
-
 function rebirth() {
-  if (score >= rebirthCost) {
-    rebirths++
-    rebirthCost = 100000000;
-    score = 0;
-    clickLevel = 1;
-    upgradeCost = 50;
-    coderCost = 1000;
-    superUpgradeCost = 750;
-    coders = 0;
-    coderLevel = 1;
-    coderUpgradeCost = 1250;
+  if (data.score >= data.rebirthCost) {
+    data.rebirths++
+    data.rebirthCost = 100000000;
+    data.score = 0;
+    data.clickLevel = 1;
+    data.upgradeCost = 50;
+    data.coderCost = 1000;
+    data.superUpgradeCost = 750;
+    data.coders = 0;
+    data.coderLevel = 1;
+    data.coderUpgradeCost = 1250;
     refreshHTML()
   }
 }
 
 function refreshHTML() {
   document.getElementById("linescoded").innerHTML = 
-  "Lines Coded:" + score; 
+  "Lines Coded:" + data.score; 
   document.getElementById("level").innerHTML = 
-  "Level:" + clickLevel;
+  "Level:" + data.clickLevel;
   document.getElementById("coderCost").innerHTML = 
-  "Cost to Buy Coders:" + coderCost;
+  "Cost to Buy Coders:" + data.coderCost;
   document.getElementById("coders").innerHTML = 
-  "Coders:" + coders;
+  "Coders:" + data.coders;
   document.getElementById("coderLevel").innerHTML = 
-  "Coder Level:" + coderLevel;
-  document.getElementById("coderUpgradeCost").innerHTML = "Cost to Upgrade Coders:" + coderUpgradeCost;
+  "Coder Level:" + data.coderLevel;
+  document.getElementById("coderUpgradeCost").innerHTML = "Cost to Upgrade Coders:" + data.coderUpgradeCost;
   document.getElementById("upgradeCost").innerHTML = 
-  "Cost to Upgrade:" + upgradeCost;
-  document.getElementById("superUpgradeCost").innerHTML = "Cost to Super Upgrade:" + superUpgradeCost;
-  document.getElementById("rebirthLevel").innerHTML =  "Rebirths:" + rebirths;
-  document.getElementById("rebirthCost").innerHTML = "Rebirth Cost:" + rebirthCost;
+  "Cost to Upgrade:" + data.upgradeCost;
+  document.getElementById("superUpgradeCost").innerHTML = "Cost to Super Upgrade:" + data.superUpgradeCost;
+  document.getElementById("rebirthLevel").innerHTML =  "Rebirths:" + data.rebirths;
+  document.getElementById("rebirthCost").innerHTML = "Rebirth Cost:" + data.rebirthCost;
+  setCookie("GameData", btoa(JSON.stringify(data)), 3650)
 }
 
 function gameLoop() {
-  score = score + coders * coderLevel * rebirths;
+  data.score = data.score + data.coders * data.coderLevel * data.rebirths;
   refreshHTML()
   setTimeout(gameLoop, 1000)
 }
